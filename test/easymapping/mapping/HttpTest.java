81fc20101a956643a978f2ff7c1060a7a49fc965
parksungho <parksungho86@gmail.com>
2014-12-28 15:49:20 +0900

    dateformat annotation add

--- test/easymapping/mapping/HttpTest.java
+++ test/easymapping/mapping/HttpTest.java
@@ -14,6 +14,8 @@ import org.mockito.Mockito;
 
 import com.google.gson.Gson;
 
+import easymapping.mapping.TestClass;
+
 public class HttpTest {
 
 	Http http;
@@ -27,6 +29,7 @@ public class HttpTest {
 		Map<String, String[]> map = gson.fromJson(gson.toJson(test), new HashMap<String, String[]>().getClass());
 		Mockito.when(request.getParameterMap()).thenReturn(map);
 		Mockito.when(request.getParameter("test")).thenReturn(gson.toJson(test));
+		Mockito.when(request.getParameter("schedule")).thenReturn("{\"time\":9,\"userId\":\"zerohouse\",\"head\":\"zdsf\",\"body\":\"asdf\",\"type\":\"운동\",\"term\":0,\"date\":\"2014-12-12\"}");
 		HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
 		http = new Http(request, response);
 
@@ -37,6 +40,11 @@ public class HttpTest {
 		TestClass test = http.getJsonObject(TestClass.class);
 		assertEquals(http.getJsonObject(TestClass.class), test);
 		assertEquals(http.getJsonObject(TestClass.class, "test"), test);
+	}
+	
+	@Test
+	public void parseTest(){
+		System.out.println(http.getJsonObject(Schedule.class,"schedule"));
 	}
 
 }
